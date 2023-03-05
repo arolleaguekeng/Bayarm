234810b50fa0a413d3a058580e70b0972dcb77b7
TIENTCHEUisrael <israel01tientcheu@gmail.com>
2023-03-05 02:18:29 +0100

    Home Finish

--- bayarm/lib/screens/home/home_content.dart
+++ bayarm/lib/screens/home/home_content.dart
@@ -152,30 +152,29 @@ class _HomeContent extends State<HomeContent> {
                 itemCount: categories.length,
               ),
             ),
-            Container(
-              height: 1000,
-              padding: const EdgeInsets.all(8.0),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
               child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    childAspectRatio: 3 / 2,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20,
-                  ),
-                  itemCount: products.length,
-                  itemBuilder: (BuildContext ctx, index) {
-                    return productWidget(
-                      product: products[index],
-                      likebtn: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.heart_broken,
-                          color: green,
-                          size: 15,
-                        ),
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    mainAxisExtent: 300),
+                itemBuilder: (_, index) {
+                  return productWidget2(
+                    product: products[index],
+                    btnicon: IconButton(
+                      icon: Icon(
+                        Icons.heart_broken,
+                        color: green,
                       ),
-                    );
-                  }),
+                      onPressed: () {},
+                    ),
+                  );
+                },
+                itemCount: products.length,
+              ),
             ),
           ],
         ),
@@ -185,119 +184,116 @@ class _HomeContent extends State<HomeContent> {
 
   Widget productWidget(
       {required Product product, required IconButton likebtn}) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(5),
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    /*side: BorderSide(
-                      color: green,
-                    ),*/
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  elevation: 5,
-                  child: InkWell(
-                    onTap: () {},
-                    child: Container(
-                      padding: const EdgeInsets.all(5),
-                      height: 210,
-                      width: MediaQuery.of(context).size.width * 0.50,
-                      child: Stack(
-                        children: [
-                          Image(
-                            image: AssetImage(
-                                "assets/images/png/" + product.imgUrl),
-                            fit: BoxFit.cover,
-                            height: 200,
-                            width: 200,
-                          ),
-                          Positioned(
-                            top: 20.0,
-                            left: 120.0,
-                            child: likebtn,
-                          ),
-                        ],
-                      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(5),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  /*side: BorderSide(
+                    color: green,
+                  ),*/
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                elevation: 5,
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.all(5),
+                    height: 210,
+                    width: MediaQuery.of(context).size.width * 0.50,
+                    child: Stack(
+                      children: [
+                        Image(
+                          image:
+                              AssetImage("assets/images/png/" + product.imgUrl),
+                          fit: BoxFit.cover,
+                          height: 200,
+                          width: 200,
+                        ),
+                        Positioned(
+                          top: 20.0,
+                          left: 140.0,
+                          child: likebtn,
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(5),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        CustumText(
-                          text: product.name,
-                          size: 14,
+            ),
+            Container(
+              padding: const EdgeInsets.all(5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      CustumText(
+                        text: product.name,
+                        size: 14,
+                        weight: FontWeight.bold,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: green,
+                        size: 12,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        child: CustumText(
+                          text: '4.8',
+                          size: 12,
                           weight: FontWeight.bold,
                         ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.star,
-                          color: green,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        height: 10,
+                        width: 2,
+                        color: Colors.black,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 5),
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: green),
+                        ),
+                        child: CustumText(
+                          text: '3405 Sold',
                           size: 12,
+                          color: green,
                         ),
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 5),
-                          child: CustumText(
-                            text: '4.8',
-                            size: 12,
-                            weight: FontWeight.bold,
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 5),
-                          height: 10,
-                          width: 2,
-                          color: Colors.black,
-                        ),
-                        Container(
-                          margin: const EdgeInsets.only(left: 5),
-                          padding: const EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: green),
-                          ),
-                          child: CustumText(
-                            text: '3405 Sold',
-                            size: 12,
-                            color: green,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    CustumText(
-                      text: product.price,
-                      size: 13,
-                      color: green,
-                      weight: FontWeight.bold,
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  CustumText(
+                    text: product.price,
+                    size: 13,
+                    color: green,
+                    weight: FontWeight.bold,
+                  ),
+                ],
               ),
-            ],
-          ),
-        ],
-      ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 
@@ -342,6 +338,120 @@ class _HomeContent extends State<HomeContent> {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class productWidget2 extends StatelessWidget {
+  final Product product;
+  final IconButton btnicon;
+  const productWidget2(
+      {super.key, required this.product, required this.btnicon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(16.0),
+              topRight: Radius.circular(16),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.all(5),
+                height: 205,
+                child: Stack(
+                  children: [
+                    Image(
+                      image: AssetImage("assets/images/png/" + product.imgUrl),
+                      fit: BoxFit.cover,
+                      height: 200,
+                      width: double.infinity,
+                    ),
+                    Positioned(
+                      top: 20.0,
+                      left: 100.0,
+                      child: btnicon,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    CustumText(
+                      text: product.name,
+                      size: 14,
+                      weight: FontWeight.bold,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: green,
+                      size: 12,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 5),
+                      child: CustumText(
+                        text: '4.8',
+                        size: 12,
+                        weight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 5),
+                      height: 10,
+                      width: 2,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 5),
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: green),
+                      ),
+                      child: CustumText(
+                        text: '3405 Sold',
+                        size: 12,
+                        color: green,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 2,
+                ),
+                CustumText(
+                  text: product.price,
+                  size: 13,
+                  color: green,
+                  weight: FontWeight.bold,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
