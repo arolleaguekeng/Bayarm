5dc6c8b2672fc6afd9c697465ec27284035d27e1
TIENTCHEUisrael <israel01tientcheu@gmail.com>
2023-03-24 07:18:28 +0100

    first finalisaiton of payment page

--- bayarm/lib/screens/paiement/paiement_content.dart
+++ bayarm/lib/screens/paiement/paiement_content.dart
@@ -1,6 +1,8 @@
-import 'package:bayarm/constants/constants.dart';
+import 'package:bayarm/notfoundPage.dart';
 import 'package:bayarm/screens/components/forms/custom_text.dart';
 import 'package:flutter/material.dart';
+
+enum Pet { wallet, paypal, googlePay, applePay, orangeM, momo }
 
 class PaiementContent extends StatefulWidget {
   const PaiementContent({super.key});
@@ -10,6 +12,7 @@ class PaiementContent extends StatefulWidget {
 }
 
 class _PaiementContentState extends State<PaiementContent> {
+  Pet _pet = Pet.momo;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,18 +23,34 @@ class _PaiementContentState extends State<PaiementContent> {
         elevation: 0,
         title: CustumText(
           text: 'Paiement Methods',
-          size: 15,
+          size: 20,
           weight: FontWeight.bold,
         ),
         backgroundColor: Colors.white,
         centerTitle: false,
         actions: [
-          IconButton(
-            icon: Icon(
-              Icons.add_box,
-              color: Colors.grey.shade400,
+          Container(
+            padding: const EdgeInsets.all(1),
+            decoration: BoxDecoration(
+              border: Border.all(),
+              borderRadius: BorderRadius.circular(20),
             ),
-            onPressed: () {},
+            child: IconButton(
+              icon: Icon(
+                Icons.add_box,
+                color: Colors.grey.shade400,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (__) {
+                      return NotFoundPage();
+                    },
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
@@ -44,7 +63,208 @@ class _PaiementContentState extends State<PaiementContent> {
               CustumText(
                 text: 'Select the payment method you want to use',
                 size: 16,
-                color: grey,
+                color: Colors.black,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 15,
+                ),
+                height: 400,
+                child: ListView(
+                  children: [
+                    Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {},
+                          child: Card(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            color: Colors.white,
+                            elevation: 1,
+                            child: ListTile(
+                              leading: Image.asset(
+                                "assets/images/png/online.png",
+                                height: 40,
+                              ),
+                              title: CustumText(
+                                text: 'My Wallet',
+                                size: 18,
+                                weight: FontWeight.bold,
+                              ),
+                              trailing: Radio<Pet>(
+                                value: Pet.wallet,
+                                groupValue: _pet,
+                                onChanged: (Pet? value) {
+                                  setState(() {
+                                    _pet = value!;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Card(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            color: Colors.white,
+                            elevation: 1,
+                            child: ListTile(
+                              leading: Image.asset(
+                                "assets/images/png/online.png",
+                                height: 40,
+                              ),
+                              title: CustumText(
+                                text: 'Paypal',
+                                size: 18,
+                                weight: FontWeight.bold,
+                              ),
+                              trailing: Radio<Pet>(
+                                value: Pet.paypal,
+                                groupValue: _pet,
+                                onChanged: (Pet? value) {
+                                  setState(() {
+                                    _pet = value!;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Card(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            color: Colors.white,
+                            elevation: 1,
+                            child: ListTile(
+                              leading: Image.asset(
+                                "assets/images/png/online.png",
+                                height: 40,
+                              ),
+                              title: CustumText(
+                                text: 'Google Pay',
+                                size: 18,
+                                weight: FontWeight.bold,
+                              ),
+                              trailing: Radio<Pet>(
+                                value: Pet.googlePay,
+                                groupValue: _pet,
+                                onChanged: (Pet? value) {
+                                  setState(() {
+                                    _pet = value!;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Card(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            color: Colors.white,
+                            elevation: 1,
+                            child: ListTile(
+                              leading: Image.asset(
+                                "assets/images/png/online.png",
+                                height: 40,
+                              ),
+                              title: CustumText(
+                                text: 'Apple Pay',
+                                size: 18,
+                                weight: FontWeight.bold,
+                              ),
+                              trailing: Radio<Pet>(
+                                value: Pet.applePay,
+                                groupValue: _pet,
+                                onChanged: (Pet? value) {
+                                  setState(() {
+                                    _pet = value!;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Card(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            color: Colors.white,
+                            elevation: 1,
+                            child: ListTile(
+                              leading: Image.asset(
+                                "assets/images/png/online.png",
+                                height: 40,
+                              ),
+                              title: CustumText(
+                                text: 'Orange Money',
+                                size: 18,
+                                weight: FontWeight.bold,
+                              ),
+                              trailing: Radio<Pet>(
+                                value: Pet.orangeM,
+                                groupValue: _pet,
+                                onChanged: (Pet? value) {
+                                  setState(() {
+                                    _pet = value!;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Card(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            color: Colors.white,
+                            elevation: 1,
+                            child: ListTile(
+                              leading: Image.asset(
+                                "assets/images/png/online.png",
+                                height: 40,
+                              ),
+                              title: CustumText(
+                                text: 'Mobile Money',
+                                size: 18,
+                                weight: FontWeight.bold,
+                              ),
+                              trailing: Radio<Pet>(
+                                value: Pet.momo,
+                                groupValue: _pet,
+                                onChanged: (Pet? value) {
+                                  setState(() {
+                                    _pet = value!;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              MaterialButton(
+                onPressed: () {},
+                height: 50,
+                elevation: 0,
+                splashColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                color: Colors.green,
+                child: Center(
+                  child: CustumText(
+                    text: "Confirm Payment",
+                    size: 20,
+                    color: Colors.white,
+                    weight: FontWeight.bold,
+                  ),
+                ),
               ),
             ],
           ),
