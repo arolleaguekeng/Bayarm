898f02d233685a227d498d9c838b280390df81d8
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2023-03-24 02:40:57 +0100

    Add country picker on edit profile.

--- bayarm/lib/main.dart
+++ bayarm/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:bayarm/routes/custum_routes.dart';
 import 'package:bayarm/screens/navigations/navigation_screen.dart';
 import 'package:bayarm/screens/profiles/profiles_content.dart';
+import 'package:bayarm/screens/profiles/profiles_screen.dart';
+import 'package:bayarm/screens/profiles/update_profile_screen.dart';
 import 'package:bayarm/screens/welcome/welcom_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+//import 'package:adaptive_theme/adaptive_theme.dart';
 
 import 'constants/constants.dart';
 
@@ -28,7 +31,7 @@ class MyApp extends StatelessWidget {
           secondaryHeaderColor: Colors.white,
           textTheme: const TextTheme(bodyText2: TextStyle(color: textColor)),
           backgroundColor: Colors.brown),
-      home: WelcomeScreen(),
+      home: UpdateProfileContent(),
       color: secondaryColor,
       onGenerateRoute: CustomRoute.allRoutes,
     );
